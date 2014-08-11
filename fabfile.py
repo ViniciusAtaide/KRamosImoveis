1f6f2b41785fe6719ebd33f9e5be79041983e034
ViniciusAtaide <viniciusataid@gmail.com>
2014-08-11 17:06:33 -0300

    fix merge

--- fabfile.py
+++ fabfile.py
@@ -14,20 +14,17 @@ pip_bin = venv_bin + 'pip'
 @hosts('yuri@107.170.80.129:8080')
 def deploy():
 	with cd(pwd):
-		run('sudo git pull')
+		run('git pull')
 
 	with cd(pwdfront):
-		run('sudo git pull')
-		run('sudo npm install')
-		run('bower install')
-		run('grunt build')
-		run('cp -avr ./dist/ ' + os.path.join(pwd,'/static/kramosfront'))
-		run('cp ./dist/index.html ' + os.path.join(pwd,'/templates/front/'))
+		run('git pull')
+		run('grunt --force')
+		run('cp -avr ./kramosfront/*' + os.path.join(pwd,'/static/front'))
+		run('cp ./kramosfront/index.html ' + os.path.join(pwd,'/templates/front'))
 
 	with cd(pwd):
 		with shell_env(DJANGO_SETTINGS_MODULE='treloso.settings_production'):
 			run('{0} install -r requeriments.txt'.format(pip_bin))
 			run('{0} manage.py syncdb'.format(python_bin))
-			run('{0} manage.py migrate'.format(python_bin))
 			run('{0} manage.py collectstatic --noinput'.format(python_bin))
-			run('{0} supervisorctl restart kleberci')
+			run('supervisorctl restart kleberci')
